8205765f4c49837e6bf88cf928927cf99df12e53
Muskan Soni <muskansoni782003@gmail.com>
2026-07-18 02:45:25 +0530

    Fix MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://muskansoni782003_db_user:xQmvBvZmEz45vuNf@cluster0.ebsdww3.mongodb.net/productivityDB?retryWrites=true&w=majority"
+      "mongodb+srv://muskansoni782003_db_user:Muskan%401234@cluster0.ebsdww3.mongodb.net/?appName=Cluster0"
     );
     console.log("MongoDB Connected ✅");
   } catch (error) {
